a81122d5b6af442be5f419adac779eea7bf0f7be
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-11-28 12:41:22 +0100

    test per attribuire nome, punti fatti e falli subiti ai team che ho creato, palesemente sbagliatissimo

---
+++ Snack-5/script.js
@@ -0,0 +1,20 @@
+// Creare un array di oggetti di squadre di volley.
+// Ogni squadra avrà diverse proprietà: nome, punti fatti e falli subiti.
+// Generare numeri random al posto delle nelle proprietà:
+// punti fatti e falli subiti
+
+const volleyTeams = [
+    'Pescara',
+    'Firenze',
+    'Bologna',
+    'Milano',
+    'Siena',
+    'Monopoli',
+    'Fidenza'
+];
+
+const teamInfos = volleyTeams.map(team => ({
+    nome: x,
+    punti fatti: x,
+    falli subiti: x,
+}));
