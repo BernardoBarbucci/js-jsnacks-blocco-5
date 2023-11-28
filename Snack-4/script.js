ef35ef6a368401bf55bf10b655384c7808285e48
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-11-28 12:23:44 +0100

    arays completati

---
+++ Snack-4/script.js
@@ -0,0 +1,18 @@
+// Scrivi una funzione che fonda due array (aventi lo stesso numero di elementi)
+// prendendo alternativamente gli elementi da uno e dall'altro
+// es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+
+const arrayFirst = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e'
+];
+const arraySecond = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5'
+];
